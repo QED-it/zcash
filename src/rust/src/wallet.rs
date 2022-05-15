fbd291e3a5d511a8c84177ff24d63ce3892e1ee5
vivek-arte <vivek@qed-it.com>
2022-05-15 20:12:06 +0530

    updated FFINoteMetadata and RawOrchardNoteMetadata to include note types

--- src/rust/src/wallet.rs
+++ src/rust/src/wallet.rs
@@ -22,6 +22,7 @@ use orchard::{
     tree::{MerkleHashOrchard, MerklePath},
     Address, Bundle, Note,
 };
+use orchard::note::NoteType;
 
 use crate::{
     builder_ffi::OrchardSpendInfo,
@@ -1025,6 +1026,7 @@ pub struct FFINoteMetadata {
     action_idx: u32,
     recipient: *mut Address,
     note_value: i64,
+    note_type: [u8; 32],        // only passing the byte representation of the type
     memo: [u8; 512],
 }
 
@@ -1050,6 +1052,7 @@ pub extern "C" fn orchard_wallet_get_filtered_notes(
             action_idx: outpoint.action_idx as u32,
             recipient: Box::into_raw(Box::new(dnote.note.recipient())),
             note_value: dnote.note.value().inner() as i64,
+            note_type: dnote.note.note_type().to_bytes(),
             memo: dnote.memo,
         };
         unsafe { (push_cb.unwrap())(result, metadata) };
@@ -1066,7 +1069,7 @@ pub struct FFIActionSpend {
     outpoint_action_idx: u32,
     received_at: *mut Address,
     value: i64,
-}
+}   //TODO: add note_type?
 
 /// A type used to pass decrypted output information across the FFI boundary.
 /// This must have the same representation as `struct RawOrchardOutputData`
@@ -1078,7 +1081,7 @@ pub struct FFIActionOutput {
     value: i64,
     memo: [u8; 512],
     is_outgoing: bool,
-}
+}   //TODO: add note_type?
 
 /// A C++-allocated function pointer that can send a FFIActionSpend value
 /// to a receiver.
