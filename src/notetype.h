f5737eb9b319cf3d06a9176ed2660549c2b9a7c2
vivek-arte <vivek@qed-it.com>
2022-05-15 11:20:14 +0530

    initial commit for note types, not operational

---
+++ src/notetype.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 QEDIT
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+#ifndef ZCASH_NOTETYPE_H
+#define ZCASH_NOTETYPE_H
+
+#include <stdlib.h>
+#include <string>
+
+#endif //ZCASH_NOTETYPE_H
