633c85a3e3aaa88a04fa785af8b8a0ea21390aa4
vivek-arte <vivek@qed-it.com>
2022-05-15 11:20:14 +0530

    initial commit for note types, not operational
    initial commit for note types, not operational
    
    initial commit for note types, not operational

---
+++ src/notetype.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2022 QEDIT Systems
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+#include "notetype.h"
